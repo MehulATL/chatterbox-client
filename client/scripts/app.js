e85d06f123322bc17112fde21120fe367924e3e8
Hack Reactor Students <mehul8@gmail.com>
2013-11-25 17:00:06 -0800

    post works, need to fix a bug in our get ajax call

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,19 @@
 // YOUR CODE HERE:
 $(document).ready(function(){
 
+  //variable declarations
   var $messageContainer = $('.messageContainer');
+  var $textbox = $('.input');
+  var username = window.location.search.slice(10);
+  console.log(username);
+
+  var userMessage ={
+    'username': username,
+    'text': "hellow",
+    'roomname': 'test'
+  };
+
+  // AJAX ISH
   var getMessages = function(){
     $.ajax({
       // always use this url
@@ -10,10 +22,13 @@ $(document).ready(function(){
       // data: data,
       contentType: 'application/json',
       success: function(data){
+        data['results'].sort(function(a,b){
+          return a.createdAt < b.createdAt ? -1:1;
+        });
         console.log(data);
         for (var i = 0; i < data.results.length; i++) {
           var message = data.results[i];
-          var dataContainer = $("<div />", {'text': message.text});
+          var dataContainer = $("<div />", {'text': message.username +': ' +message.text});
           //console.log(dataContainer);
           $(dataContainer).appendTo($messageContainer);
         }
@@ -27,9 +42,38 @@ $(document).ready(function(){
 
   getMessages();
 
+  var postMessages = function(message){
+    $.ajax({
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(userMessage);
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  };
+
+
+  // Button ish.
   $('.refresh').on('click', function(){
     $messageContainer.html('');
     getMessages();
   });
+
+  $('.send').on('click', function(){
+    //console.log($textbox.val());
+    postMessages(userMessage);
+    $textbox.val('');
+    //$('.refresh').trigger();
+  });
+
+
 
 });
