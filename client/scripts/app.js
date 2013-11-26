16629f19d8f015a38829b6d26bd6c07a3f3cd638
Hack Reactor Students <mehul8@gmail.com>
2013-11-25 20:06:29 -0800

    initial attempt at room functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,34 +3,33 @@ $(document).ready(function(){
 
   //variable declarations
   var $messageContainer = $('.messageContainer');
-  var $textbox = $('.input');
+  //var $textbox = $('.input');
   var username = window.location.search.slice(10);
-  console.log(username);
+  //console.log(username);
+  //console.log(messageValue);
 
-  var userMessage ={
-    'username': username,
-    'text': "hellow",
-    'roomname': 'test'
-  };
+  //var room = prompt("What room would you like to enter?");
+
+
+
 
   // AJAX ISH
   var getMessages = function(){
     $.ajax({
       // always use this url
-      url: 'https://api.parse.com/1/classes/chatterbox',
+      url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=25', //&where={"room":'+ '"'+ room + '"' + '}' ,
       type: 'GET',
-      // data: data,
       contentType: 'application/json',
       success: function(data){
-        data['results'].sort(function(a,b){
-          return a.createdAt < b.createdAt ? -1:1;
-        });
-        console.log(data);
         for (var i = 0; i < data.results.length; i++) {
           var message = data.results[i];
-          var dataContainer = $("<div />", {'text': message.username +': ' +message.text});
+          var rooms = data.results[i].roomname;
+          var uniqueRooms = $("<li />", {'text':_.unique(rooms) });
+          console.log(rooms);
+          var dataContainer = $("<div />", {'text': message.username + ': ' + message.text});
           //console.log(dataContainer);
           $(dataContainer).appendTo($messageContainer);
+          uniqueRooms.appendTo('ul');
         }
       },
       error: function (data) {
@@ -62,18 +61,31 @@ $(document).ready(function(){
 
 
   // Button ish.
-  $('.refresh').on('click', function(){
+$('.refresh').click(function(){
     $messageContainer.html('');
     getMessages();
   });
 
+  // setInterval(showMessages, 500);
+
+  var userMessage = {
+    'username': username,
+    'text': 'string',
+    'roomname': 'hackreactor'
+  };
+
   $('.send').on('click', function(){
-    //console.log($textbox.val());
+    userMessage.text = $('.input').val();
     postMessages(userMessage);
-    $textbox.val('');
-    //$('.refresh').trigger();
+    $('.input').val('');
+    $('.refresh').click();
   });
 
+  $('.input').keyup(function(e){
+    if(e.keyCode === 13){
+      $('.send').click();
+    }
+  });
 
 
 });
