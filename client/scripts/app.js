ef1acf2cff1f6455bd29b9c63205e4fc9708130b
Hack Reactor Students <mehul8@gmail.com>
2013-11-26 11:35:31 -0800

    cleaned up code and added some comments.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,49 +1,45 @@
 // YOUR CODE HERE:
 $(document).ready(function(){
 
-  //variable declarations
+  // our variable declarations
   var $messageContainer = $('.messageContainer');
-  //var $textbox = $('.input');
+  var $textbox = $('.input');
   var username = window.location.search.slice(10);
-  //console.log(username);
-  //console.log(messageValue);
   var message;
-
-  //var room = prompt("What room would you like to enter?");
-
-
-
 
   // AJAX ISH
   var getMessages = function(){
     $.ajax({
-      // always use this url
       url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=25', //&where={"room":'+ '"'+ room + '"' + '}' ,
       type: 'GET',
       contentType: 'application/json',
       success: function(data){
         var roomFilter = [];
+        // loop through our data to separate messages.
         for (var i = 0; i < data.results.length; i++) {
           message = data.results[i];
+          // putting a limit on message length
           if(data.results[i].text.length > 100) {
             message.text = "I suck";
             console.log(data.results[i]);
           }
+          // making a list of all unique chatrooms
           var rooms = data.results[i].roomname;
           if(roomFilter.indexOf(rooms) === -1){
             roomFilter.push(rooms);
           }
+          // constructing and appending our message dom nodes
           var dataContainer = $("<div />", {'text': message.username + ': ' + message.text, 'class': 'messageClass ' + message.roomname + ' ' + message.username, 'username':message.username});
           console.log(data);
           $(dataContainer).appendTo($messageContainer);
         }
+        // creating and appending our chatroom nodes to the dropdown menu
         for (var j = 0; j < roomFilter.length; j++){
           var roomNodes = $("<option />", {'text': roomFilter[j] });
           roomNodes.appendTo('select');
         }
       },
       error: function (data) {
-        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to get message');
       }
     });
@@ -53,7 +49,6 @@ getMessages();
 
 var postMessages = function(message){
   $.ajax({
-      // always use this url
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'POST',
       data: JSON.stringify(message),
@@ -63,41 +58,41 @@ var postMessages = function(message){
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
-        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message');
       }
     });
 };
 
 
-  // Button ish.
+  // show new messages
   $('.refresh').click(function(){
     $messageContainer.html('');
     $('select').html('');
     getMessages();
   });
 
-  // setInterval(showMessages, 500);
-
+  // user message object in the format that our API accepts
   var userMessage = {
     'username': username,
     'text': 'string',
     'roomname': 'hackreactor'
   };
 
+  // send message function
   $('.send').on('click', function(){
-    userMessage.text = $('.input').val();
+    userMessage.text = $textbox.val();
     postMessages(userMessage);
-    $('.input').val('');
+    $textbox.val('');
     $('.refresh').click();
   });
 
-  $('.input').keyup(function(e){
+  $textbox.keyup(function(e){
     if(e.keyCode === 13){
       $('.send').click();
     }
   });
 
+  // chat room selection function
   $('select').change(function(){
     var roomSelection = '';
     $('select option:selected').each(function(){
@@ -107,20 +102,12 @@ var postMessages = function(message){
     $('.messageClass').not( '.' + roomSelection).hide();
   });
 
-
+  // add/remove friend function
   $('body').on('click', '.messageClass',function(){
     var userSelection = $(this).attr('username');
     console.log(userSelection);
     $('.' + userSelection).toggleClass('bold');
-
   });
 
-/* TODO --
-* add eventlisteners to the chatroom dropdown that will call a function that will filter the chat results.
-* for friends just addClass() and that other shit.
-* ask Linzay how she did her filtering process.
-
-
-*/
 
 });
