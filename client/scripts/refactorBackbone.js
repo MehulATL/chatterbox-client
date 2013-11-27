105ec50f74ae4eac68bb34fc63f9985d2d538182
Hack Reactor Students <mehul8@gmail.com>
2013-11-26 21:46:57 -0800

    decoupled our jquery from the document ready. models and views ready.

--- client/scripts/refactorBackbone.js
+++ client/scripts/refactorBackbone.js
@@ -1,38 +1,52 @@
+//events = Backbone.Events();
+
 // YOUR CODE HERE:
-$(document).ready(function(){
+var Messages = function(){
+  this.$messageContainer = $('.messageContainer');
+  this.get();
+ };
 
-  // our variable declarations
-  var $messageContainer = $('.messageContainer');
-  var $textbox = $('.input');
-  var username = window.location.search.slice(10);
-  var message;
+Messages.prototype.add = function (options){
+  // debugger;
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(options),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
 
-  // AJAX ISH
-  var getMessages = function(){
-    $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=25',
-      type: 'GET',
-      contentType: 'application/json',
-      success: function(data){
-        var roomFilter = [];
-      // loop through our data to separate messages.
-      for (var i = 0; i < data.results.length; i++) {
-        message = data.results[i];
-        // putting a limit on message length
-        // if(data.results[i].text.length > 100) {
-        //   message.text = "I suck";
-        //   console.log(data.results[i]);
-        // }
-        // making a list of all unique chatrooms
-        var rooms = data.results[i].roomname;
-        if(roomFilter.indexOf(rooms) === -1){
-          roomFilter.push(rooms);
+// AJAX ISH
+Messages.prototype.get = function(option){
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=25',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function(data){
+      var roomFilter = [];
+        // loop through our data to separate messages.
+        for (var i = 0; i < data.results.length; i++) {
+          message = data.results[i];
+          // putting a limit on message length
+          // if(data.results[i].text.length > 100) {
+          //   message.text = "I suck";
+          //   console.log(data.results[i]);
+          // }
+          // making a list of all unique chatrooms
+          var rooms = data.results[i].roomname;
+          if(roomFilter.indexOf(rooms) === -1){
+            roomFilter.push(rooms);
+          }
+          // constructing and appending our message dom nodes
+          var dataContainer = $("<div />", {'text': message.username + ': ' + message.text, 'class': 'messageClass ' + message.roomname + ' ' + message.username, 'username':message.username});
+          $(dataContainer).appendTo($('.messageContainer'));
         }
-        // constructing and appending our message dom nodes
-        var dataContainer = $("<div />", {'text': message.username + ': ' + message.text, 'class': 'messageClass ' + message.roomname + ' ' + message.username, 'username':message.username});
-        console.log(data);
-        $(dataContainer).appendTo($messageContainer);
-      }
       // creating and appending our chatroom nodes to the dropdown menu
       for (var j = 0; j < roomFilter.length; j++){
         var roomNodes = $("<option />", {'text': roomFilter[j] });
@@ -45,30 +59,13 @@ $(document).ready(function(){
   });
 };
 
-getMessages();
-
-var postMessages = function(message){
-  $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      console.error('chatterbox: Failed to send message');
-    }
-  });
-};
 
 
-  // show new messages
-  $('.refresh').click(function(){
-    $messageContainer.html('');
-    $('select').html('');
-    getMessages();
-  });
+var NewMessageView = function(options){
+  var $messageContainer = $('.messageContainer');
+  var $textbox = $('.input');
+  var username = window.location.search.slice(10);
+  var message;
 
   // user message object in the format that our API accepts
   var userMessage = {
@@ -77,10 +74,18 @@ var postMessages = function(message){
     'roomname': 'hackreactor'
   };
 
+  // show new messages
+  $('.refresh').click(function(){
+    $messageContainer.html('');
+    $('select').html('');
+    Messages.prototype.get();
+  });
+
   // send message function
   $('.send').on('click', function(){
     userMessage.text = $textbox.val();
-    postMessages(userMessage);
+    debugger;
+    Messages.prototype.add(userMessage);
     $textbox.val('');
     $('.refresh').click();
   });
@@ -104,9 +109,12 @@ var postMessages = function(message){
   // add/remove friend function
   $('body').on('click', '.messageClass',function(){
     var userSelection = $(this).attr('username');
-    console.log(userSelection);
     $('.' + userSelection).toggleClass('bold');
   });
+};
 
+$(document).ready(function(){
 
+  var messages = new Messages();
+  new NewMessageView({messages: messages});
 });
