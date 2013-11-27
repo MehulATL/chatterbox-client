5e4bf582468832774977f5a9d2b22f89fd8e55bc
Mehul Patel <mehul8@gmail.com>
2013-11-26 18:43:16 -0800

    refactored code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,42 +7,47 @@ $(document).ready(function(){
   var username = window.location.search.slice(10);
   var message;
 
-  // AJAX ISH
-  var getMessages = function(){
-    $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=25', //&where={"room":'+ '"'+ room + '"' + '}' ,
-      type: 'GET',
-      contentType: 'application/json',
-      success: function(data){
-        var roomFilter = [];
-        // loop through our data to separate messages.
-        for (var i = 0; i < data.results.length; i++) {
-          message = data.results[i];
-          // putting a limit on message length
-          if(data.results[i].text.length > 100) {
-            message.text = "I suck";
-            console.log(data.results[i]);
-          }
-          // making a list of all unique chatrooms
-          var rooms = data.results[i].roomname;
-          if(roomFilter.indexOf(rooms) === -1){
-            roomFilter.push(rooms);
-          }
-          // constructing and appending our message dom nodes
-          var dataContainer = $("<div />", {'text': message.username + ': ' + message.text, 'class': 'messageClass ' + message.roomname + ' ' + message.username, 'username':message.username});
-          console.log(data);
-          $(dataContainer).appendTo($messageContainer);
+  // getMessages success
+  var getSuccess = function(data){
+    var roomFilter = [];
+      // loop through our data to separate messages.
+      for (var i = 0; i < data.results.length; i++) {
+        message = data.results[i];
+        // putting a limit on message length
+        if(data.results[i].text.length > 100) {
+          message.text = "I suck";
+          console.log(data.results[i]);
         }
-        // creating and appending our chatroom nodes to the dropdown menu
-        for (var j = 0; j < roomFilter.length; j++){
-          var roomNodes = $("<option />", {'text': roomFilter[j] });
-          roomNodes.appendTo('select');
+        // making a list of all unique chatrooms
+        var rooms = data.results[i].roomname;
+        if(roomFilter.indexOf(rooms) === -1){
+          roomFilter.push(rooms);
         }
-      },
-      error: function (data) {
-        console.error('chatterbox: Failed to get message');
+        // constructing and appending our message dom nodes
+        var dataContainer = $("<div />", {'text': message.username + ': ' + message.text, 'class': 'messageClass ' + message.roomname + ' ' + message.username, 'username':message.username});
+        console.log(data);
+        $(dataContainer).appendTo($messageContainer);
       }
-    });
+      // creating and appending our chatroom nodes to the dropdown menu
+    for (var j = 0; j < roomFilter.length; j++){
+      var roomNodes = $("<option />", {'text': roomFilter[j] });
+      roomNodes.appendTo('select');
+    }
+  }
+
+  // AJAX ISH
+var getMessages = function(){
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=25',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function(data){
+      getSuccess(data)
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to get message');
+    }
+  });
 };
 
 getMessages();
@@ -54,7 +59,6 @@ var postMessages = function(message){
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log(userMessage);
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
